8412ebfa8089d21e42ad0cbebaa0be8a255a6719
kelvinharron <kelvinharron@outlook.com>
2016-09-06 02:57:00 +0100

    added offline validation for login and register views
    commenting

--- majorprojectios/ViewController.swift
+++ majorprojectios/ViewController.swift
@@ -1,47 +1,40 @@
-//
-//  ViewController.swift
-//  majorprojectios
-//
-//  Created by Kelvin Harron on 29/08/2016.
-//  Copyright © 2016 Kelvin Harron. All rights reserved.
-//
-
 import UIKit
 import Alamofire
 
-
+/// Start point of our application. First view from the storyboard we see is the welcome screen which the following code handles
+///
+///
 class ViewController: UIViewController {
     
+    
+    /// Label from view controller that is used to display server status
     @IBOutlet weak var serverStatus: UILabel!
+    /// Status URL returns a 200 if operational, used to update status label
     let statusURL = "http://localhost:54321/status"
     
+    /// Swift lifecycle function, called only once when the view is
     override func viewDidLoad() {
         super.viewDidLoad()
         checkServerStatus()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+    /// Function completes a GET request to the service using Alamofire to verify if the service is operational. 
+    /// Using Alamofire built in validate function we can pull out a .Success and .Failure and switch on result
+    ///Label colour updated depending on request result
     func checkServerStatus(){
-        Alamofire.request(
-            .GET,
-            statusURL).validate().responseJSON { response in
-                
-                switch response.result {
-                case .Success(let response):
-                    print(response)
-                    self.serverStatus.text = "Online"
-                    self.serverStatus.textColor = UIColor.greenColor()
-                    break
-                case .Failure(let error):
-                    print(error)
-                    self.serverStatus.text = "Offline"
-                    self.serverStatus.textColor = UIColor.redColor()
-                    break
-                }
+        Alamofire.request(.GET, statusURL).validate().responseJSON { serverResponse in
+            switch serverResponse.result {
+            case .Success(let response):
+                print("Server operational") // console print
+                self.serverStatus.text = "Online"
+                self.serverStatus.textColor = UIColor.greenColor()
+                break
+            case .Failure(let error):
+                print("No response - server offline") // console print
+                self.serverStatus.text = "Offline"
+                self.serverStatus.textColor = UIColor.redColor()
+                break
+            }
         }
     }
 }
