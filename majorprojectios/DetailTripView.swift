ce50bc70a0e2e56fab07b960a3d2293137f65913
kelvinharron <kelvinharron@outlook.com>
2016-09-14 20:45:04 +0100

    trip functionality

---
+++ majorprojectios/DetailTripView.swift
@@ -0,0 +1,12 @@
+import Foundation
+import UIKit
+
+class DetailTripView: UIViewController {
+    
+    var toPass:String!
+    
+    override func viewDidLoad() {
+        print(toPass)
+    }
+    
+}
